f1d5c0696628c4bebf2b5a0698603655c10dd402
Dean Kinyua <kinyuadean@gmail.com>
2025-02-13 19:59:45 +0300

    add credo and sobeblow

--- mix.exs
+++ mix.exs
@@ -32,6 +32,8 @@ defmodule InvoiceGenerator.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.7.18"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
