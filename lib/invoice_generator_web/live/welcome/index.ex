4783abe49b2367b1c098ed4435bef0b223346fb0
Dean Kinyua <kinyuadean@gmail.com>
2025-02-14 13:30:21 +0300

    setup welcome page layout

--- lib/invoice_generator_web/live/welcome/index.ex
+++ lib/invoice_generator_web/live/welcome/index.ex
@@ -8,7 +8,23 @@ defmodule InvoiceGeneratorWeb.WelcomeLive.Index do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col mono-regular items-center justify-center center-element max-w-4xl">
+    <div class="flex flex-col items-center border border-red-400">
+      <section class="flex flex-col items-center justify-between gap-10 border border-red-400 h-screen py-12">
+        <div>
+          <section>logo</section>
+          <section>Invoice</section>
+        </div>
+        <div>Sign in to Invoice</div>
+        <div>
+          Sign in with Google
+        </div>
+        <div>Continue with email</div>
+        <div>
+          By creating an account, you agree to Invoice company's
+          <span class="font-bold">Terms of use</span>
+          and <span class="font-bold">Privacy Policy</span>
+        </div>
+      </section>
     </div>
     """
   end
